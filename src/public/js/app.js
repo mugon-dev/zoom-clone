e544d4f9da0e5a41e21abb7fcc6bf93cd54b36a0
conny <jmg41490400@gmail.com>
2021-10-29 15:56:13 +0900

    채팅 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,6 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 // front에서 back으로 소켓 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
 
@@ -13,6 +16,10 @@ socket.addEventListener("close", (message)=>{
     console.log("Disconnected from Server");
 });
 
-setTimeout(()=>{
-    socket.send("hello from the browser!");
-},5000);
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+messageForm.addEventListener("submit",handleSubmit);
--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.get("/", (req,res) => res.render("home"));
 // 어떤 url을 입력하던지 home("/")으로 이동
 app.get("/*",(req, res) => res.redirect("/"));
 
-const handelListen = () => console.log(`Listening on http://localhost:3000`);
+const handelListen = () => console.log(`Listening on http://localhost:3001`);
 // express 방식
 // app.listen(3000,handelListen);
 
@@ -25,15 +25,19 @@ const server = http.createServer(app);
 // WebSocket 사용 : http, websocket 서버 둘다 같은 port에서 작동
 const wss = new WebSocket.Server({server});
 
+// 소켓 저장용 더미 데이터베이스
+const sockets = [];
+
 // on method에서는 event(connection)가 발동하는 것을 기다림
 // socket에는 연결된 사람의 정보를 가져옴
 wss.on("connection", (socket)=>{
+    sockets.push(socket);
     console.log("Connect to Browser");
     socket.on("close",()=>console.log(("Disconnected from Browser")));
     socket.on("message",(message)=>{
-        console.log(Buffer.from(message, "base64").toString("utf-8"));
+        sockets.forEach(aSocket => aSocket.send(Buffer.from(message, "base64").toString("utf-8")) );
     });
     socket.send("hello");
 });
 
-server.listen(3000,handelListen);
+server.listen(3001,handelListen);
