f9a8a5b0ebbbe9f28efb0260b462920b10483c9f
mugon-dev <jmg41490400@gmail.com>
2021-10-30 17:01:16 +0900

    socket io init setting

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -17,38 +17,15 @@ app.get("/", (req, res) => res.render("home"));
 // 어떤 url을 입력하던지 home("/")으로 이동
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handelListen = () => console.log(`Listening on http://localhost:3001`);
+
 // express 방식
 // app.listen(3000,handelListen);
 
 // node에 내장된 서버 사용
-const server = http.createServer(app);
-// WebSocket 사용 : http, websocket 서버 둘다 같은 port에서 작동
-const wss = new WebSocket.Server({server});
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-// 소켓 저장용 더미 데이터베이스
-const sockets = [];
 
-// on method에서는 event(connection)가 발동하는 것을 기다림
-// socket에는 연결된 사람의 정보를 가져옴
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon"
-    console.log("Connect to Browser");
-    socket.on("close", () => console.log(("Disconnected from Browser")));
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        console.log(message);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
-                break
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break
-        }
-    });
-    socket.send("hello");
-});
-
-server.listen(3001, handelListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer
+    .listen(3000, handleListen);
