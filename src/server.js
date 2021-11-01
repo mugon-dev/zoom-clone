8e197366b090e16ae73121fa2bb3e6234b807a70
conny <jmg41490400@gmail.com>
2021-11-01 17:38:15 +0900

    만들어져 있는 방 확인, 삭제 확인

--- src/server.js
+++ src/server.js
@@ -24,6 +24,25 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+// public room 가져오기
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  // 위와 동일
+  // const sids = wsServer.sockets.adapter.sids;
+  // const rooms = wsServer.sockets.adapter.rooms;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
   // middleware
   // socket에 있는 모든 event를 살핌
@@ -40,10 +59,19 @@ wsServer.on("connection", (socket) => {
     done();
     // "welcome"이벤트를 roomName에 있는 모든 사람들에게 emit
     socket.to(roomName).emit("welcome", socket.nickname);
+
+    // 새로운 방이 만들어 졌다고 연결된 모든 socket에게 알려주기
+    wsServer.sockets.emit("room_change", publicRooms());
+
     // 접속은 끊겼지만 완전히 나가지 않은 상태
     socket.on("disconnecting", () => {
       socket.rooms.forEach((room) =>
         socket.io(room).emit("bye", socket.nickname)
+      );
+    });
+    socket.on("disconnect", () => {
+      socket.rooms.forEach((room) =>
+        wsServer.sockets.emit("room_change", publicRooms())
       );
     });
     socket.on("new_message", (msg, room, done) => {
